742987821c8791762f93ab8a1a7c53652c69ceba
akhilkk0803 <97751536+akhilkk0803@users.noreply.github.com>
2023-08-22 19:25:25 +0530

    Update Prims.cpp

--- Prims.cpp
+++ Prims.cpp
@@ -1,3 +1,42 @@
+// // list
+// #include<bits/stdc++.h>
+// using namespace std;
+// int main(){
+// priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>>q;
+// int v,e;
+// cout<<"Enter number of vertices and edges";
+// cin>>v>>e;
+// vector<pair<int,int>>adj[v];
+// for(int i=0;i<e;i++){
+// int u,v,cost;
+// cout<<"Enter edge between u and v and cost between u and v";
+// cin>>u>>v>>cost;
+// adj[u].push_back({v,cost});
+// adj[v].push_back({u,cost});
+// }
+// vector<int>visited(v,0);
+// q.push({0,0});
+// int sum=0;
+// while(!q.empty()){
+// int currNode=q.top().second;
+// int currWeight=q.top().first;
+// q.pop();
+// if(!visited[currNode]){
+// sum+=currWeight;
+// }
+// else continue;
+// visited[currNode]=1;
+// for(auto x:adj[currNode]){
+// int node=x.first;
+// int wt=x.second;
+// if(!visited[node]){
+// q.push({wt,node});
+// }
+// }
+// }
+// cout<<"The sum is "<<sum;
+// }
+//matrix
 #include<bits/stdc++.h>
 using namespace std;
 int main(){
@@ -5,13 +44,13 @@ priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>>q;
 int v,e;
 cout<<"Enter number of vertices and edges";
 cin>>v>>e;
-vector<pair<int,int>>adj[v];
+int adj[v][v]={0};
 for(int i=0;i<e;i++){
 int u,v,cost;
 cout<<"Enter edge between u and v and cost between u and v";
 cin>>u>>v>>cost;
-adj[u].push_back({v,cost});
-adj[v].push_back({u,cost});
+adj[u][v]=cost;
+adj[v][u]=cost;
 }
 vector<int>visited(v,0);
 q.push({0,0});
@@ -25,11 +64,12 @@ sum+=currWeight;
 }
 else continue;
 visited[currNode]=1;
-for(auto x:adj[currNode]){
-int node=x.first;
-int wt=x.second;
-if(!visited[node]){
-q.push({wt,node});
+for(int i=0;i<v;i++){
+int wt=adj[currNode][i];
+if(wt!=0){
+    if(!visited[i]){
+q.push({wt,i});
+}
 }
 }
 }
